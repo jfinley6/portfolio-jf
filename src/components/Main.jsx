fffe2857d3f7d8d5fc23e5e1caebc116af8c46e4
johnfinley <johnfinleybremerton@gmail.com>
2023-09-11 17:15:29 -0600

    Added Container and MainContainer styled divs to return statement

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import HomeButton from "../subComponents/HomeButton";
+
+const MainContainer = styled.div`
+  background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+
+  position: relative;
+
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
+
+const Container = styled.div`
+  padding: 2rem;
+`;
 
 const Main = () => {
-    return (
-        <div>Main Component/Page</div>
-    )
-}
+  return (
+    <MainContainer>
+      <Container>
+        <HomeButton />
+      </Container>
+    </MainContainer>
+  );
+};
 
-export default Main
+export default Main;
