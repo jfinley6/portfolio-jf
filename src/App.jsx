93827cf72cb72a297eb4f6f86df9f88f2323b12d
johnfinley <johnfinleybremerton@gmail.com>
2023-09-10 19:41:28 -0600

    Added createBrowserRouter with paths and elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./globalStyles";
 import { lightTheme } from "./components/Themes";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./components/Main"
+import AboutPage from "./components/AboutPage"
+import BlogPage from "./components/BlogPage"
+import WorkPage from "./components/WorkPage"
+import MySkillsPage from "./components/MySkillsPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/blog",
+    element: <BlogPage />,
+  },
+  {
+    path: "/work",
+    element: <WorkPage />,
+  },
+  {
+    path: "/skills",
+    element: <MySkillsPage />,
+  },
+]);
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={lightTheme}>
-        App File
+        <RouterProvider router={router} />
       </ThemeProvider>
     </>
   );
