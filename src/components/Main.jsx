4918b59d357d473a348690ae78b4cc31f19f00fc
johnfinley <johnfinleybremerton@gmail.com>
2023-09-13 16:10:49 -0600

    Added a DarkDiv with transitions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -102,6 +102,18 @@ const Center = styled.button`
   }
 `;
 
+const DarkDiv = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 50%;
+  width: ${(props) => (props.click ? "50%" : "0%")};
+  height: ${(props) => (props.click ? "100%" : "0%")};
+  background-color: #000;
+  z-index: 1;
+  transition: height 0.5s ease, width 1s ease 0.5s;
+`;
+
 const Main = () => {
   const [click, setClick] = useState(false);
 
@@ -115,6 +127,8 @@ const Main = () => {
         <HomeButton />
         <LogoComponent />
         <SocialIcons />
+
+        <DarkDiv click={click} />
 
         <Center click={click}>
           <CenterAnimation onClick={() => handleClick()} stroke="#808080" />
