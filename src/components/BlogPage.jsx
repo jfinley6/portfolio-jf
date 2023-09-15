9c58269321aae28f7a7f85d498c05b9807f15a57
johnfinley <johnfinleybremerton@gmail.com>
2023-09-15 15:54:30 -0600

    Started to file out the blog page

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import img from "/blog-bg.jpg";
+import { LogoComponent } from "../subComponents/LogoComponent";
+import HomeButton from "../subComponents/HomeButton";
+import SocialIcons from "../subComponents/SocialIcons";
+
+const MainContainer = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  height: auto;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 
 const BlogPage = () => {
-    return (
-        <div>BlogPage Component/Page</div>
-    )
-}
+  return (
+    <MainContainer>
+      <Container>
+        <HomeButton />
+        <LogoComponent />
+        <SocialIcons />
 
-export default BlogPage
+        <Center>
+          <Grid>
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
+  );
+};
+
+export default BlogPage;
