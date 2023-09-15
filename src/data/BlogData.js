c2868809ca33baf4f7c7d445c1c5f60ce82d95b8
johnfinley <johnfinleybremerton@gmail.com>
2023-09-15 16:33:05 -0600

    Filled in data from personal dev.to account

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,1 +1,29 @@
-export const Blogs = []
+export const Blogs = [
+  {
+    id: 1,
+    name: "Basics of Local Storage",
+    tags: ["javascript", "beginners", "learning", "computerscience"],
+    date: "4 Aug, 2022",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--YUN6ynyq--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/yzr72d10jr9u1qrhgdrq.png",
+    link: "https://dev.to/jtfinley/basics-of-local-storage-4l87",
+  },
+  {
+    id: 2,
+    name: "Rails Command Line Tips and Tricks",
+    tags: ["ruby", "rails", "beginners", "computerscience"],
+    date: "6 Oct, 2022",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--IyI9LtKg--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pdfwqpwb4b2izik2iobj.png",
+    link: "https://dev.to/jtfinley/rails-command-line-tips-and-tricks-kgd",
+  },
+  {
+    id: 3,
+    name: "Dynamic Page Titles in Ruby on Rails",
+    tags: ["ruby", "rails", "beginners", "tutorial"],
+    date: "29 Nov, 2022",
+    imgSrc:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--LczzsNgS--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/jqlmqbbg9cmjmmxdul8v.png",
+    link: "https://dev.to/jtfinley/dynamic-page-titles-in-ruby-on-rails-4e29",
+  },
+];
