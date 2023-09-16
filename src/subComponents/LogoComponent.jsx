0a9d99fe515e5a8d5254c718b7ffde6fc7ccaf9d
johnfinley <johnfinleybremerton@gmail.com>
2023-09-16 14:00:24 -0600

    Added media query for font-size and positioning

--- src/subComponents/LogoComponent.jsx
+++ src/subComponents/LogoComponent.jsx
@@ -4,14 +4,19 @@ import { darkTheme } from "../components/Themes";
 
 const Logo = styled.h1`
   display: inline-block;
-  color: ${(props) => props.color === 'darkTheme' ? darkTheme.text : darkTheme.body};
-  font-family: 'Pacifico', cursive;
+  color: ${(props) =>
+    props.color === "darkTheme" ? darkTheme.text : darkTheme.body};
+  font-family: "Pacifico", cursive;
 
   position: fixed;
   left: 2rem;
   top: 2rem;
   z-index: 3;
 
+  @media (max-width: 768px) {
+    left: 1.25rem;
+    font-size: 1.5rem;
+  }
 `;
 
 export const LogoComponent = (props) => {
