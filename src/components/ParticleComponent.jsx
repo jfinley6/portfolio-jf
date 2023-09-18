83f624660fdb48b336f4867db442730772f07911
johnfinley <johnfinleybremerton@gmail.com>
2023-09-18 15:54:29 -0600

    Component now accepts particles as a prop

--- src/components/ParticleComponent.jsx
+++ src/components/ParticleComponent.jsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import styled from "styled-components";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
+import { loadFull } from "tsparticles";
 
 const Box = styled.div`
   position: absolute;
@@ -13,99 +14,61 @@ const Box = styled.div`
 `;
 
 const ParticleComponent = (props) => {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadSlim(engine);
+  const { particles } = props;
+  const options = useMemo(() => {
+    return {
+      background: {
+        color: {
+          value: `${props.theme.body}`,
+        },
+        image: "",
+        position: "",
+        repeat: "",
+        size: "",
+        opacity: 1,
+      },
+      backgroundMask: {
+        composite: "destination-out",
+        cover: {
+          color: {
+            value: `${props.theme.body}`,
+          },
+          opacity: 1,
+        },
+        enable: false,
+      },
+      defaultThemes: {},
+      delay: 0,
+      duration: 0,
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: particles,
+      detectRetina: true,
+    };
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
   }, []);
-
 
   return (
     <Box>
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          background: {
-            color: {
-              value: `${props.theme.body}`,
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: `${props.theme.text}`,
-            },
-            links: {
-              color: `${props.theme.text}`,
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
+      <Particles init={particlesInit} options={options} />
     </Box>
   );
 };
 
 export default ParticleComponent;
-
