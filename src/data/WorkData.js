ea00bbb1d4a0ffaa7025f5aaf7323e09752a8717
johnfinley <johnfinleybremerton@gmail.com>
2023-09-19 18:10:04 -0600

    Started to fill in examples of my work

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,1 +1,32 @@
-export const Work = []
+export const Work = [
+  {
+    id: 1,
+    name: "Out of Bounds Discs",
+    description:
+      "A disc golf e-commerce store built using React and Ruby on Rails",
+    tags: ["react", "ruby on rails", "bootstrap"],
+
+    demo: "https://outofboundsdiscs.netlify.app/",
+    github: "https://github.com/jfinley6/disc-golf-store-frontend",
+  },
+  {
+    id: 1,
+    name: "React Weather Browser",
+    description:
+      "A weather browser built with React and incorporating Google's autocomplete API",
+    tags: ["react", "tailwind", "googleapi"],
+
+    demo: "https://outofboundsdiscs.netlify.app/",
+    github: "https://github.com/jfinley6/disc-golf-store-frontend",
+  },
+  {
+    id: 1,
+    name: "DiscussionHub",
+    description:
+      "A discussion forum where you can create communities, posts, and comments",
+    tags: ["ruby on rails", "bootstrap", "devise"],
+
+    demo: "https://discussionhub-51a7c53ae2c9.herokuapp.com/",
+    github: "https://github.com/jfinley6/DiscussionHub",
+  },
+];
