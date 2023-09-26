af6100a8d5a37531c88e25a3bf7dffce8e136ca9
johnfinley <johnfinleybremerton@gmail.com>
2023-09-25 19:28:43 -0600

    Imported and added AnimatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./globalStyles";
 import { lightTheme } from "./components/Themes";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from "./components/Main"
-import AboutPage from "./components/AboutPage"
-import BlogPage from "./components/BlogPage"
-import WorkPage from "./components/WorkPage"
-import MySkillsPage from "./components/MySkillsPage"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import MySkillsPage from "./components/MySkillsPage";
+import { AnimatePresence } from "framer-motion";
 
 const router = createBrowserRouter([
   {
@@ -38,9 +36,11 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={lightTheme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <AnimatePresence mode="wait">
+        <ThemeProvider theme={lightTheme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </AnimatePresence>
     </>
   );
 }
