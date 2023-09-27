ca285fedb3a182a11477e16792b0a327f2a65f4a
johnfinley <johnfinleybremerton@gmail.com>
2023-09-27 16:27:49 -0600

    Added framer-motion animations to Line and icon divs

--- src/subComponents/SocialIcons.jsx
+++ src/subComponents/SocialIcons.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { GitHubBtn, LinkedInBtn, ResumeBtn } from "../components/AllSvgs";
 import styled from "styled-components";
 import { darkTheme } from "../components/Themes";
-import Resume from "/JohnFinleyResume.pdf"
+import Resume from "/JohnFinleyResume.pdf";
+import { motion } from "framer-motion";
 
 const Icons = styled.div`
   display: flex;
@@ -34,7 +35,7 @@ const Icons = styled.div`
   }
 `;
 
-const Line = styled.span`
+const Line = styled(motion.span)`
   width: 2px;
   height: 8rem;
   background-color: ${(props) =>
@@ -44,29 +45,77 @@ const Line = styled.span`
 const SocialIcons = (props) => {
   return (
     <Icons>
-      <Link to="https://github.com/jfinley6" target="_blank">
-        <GitHubBtn
-          width={25}
-          height={25}
-          fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
-        />
-      </Link>
-      <Link to="https://linkedin.com/in/john-tyler-finley" target="_blank">
-        <LinkedInBtn
-          width={25}
-          height={25}
-          fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
-        />
-      </Link>
-      <Link to={Resume} target="_blank">
-        <ResumeBtn
-          width={25}
-          height={25}
-          fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
-        />
-      </Link>
+      <motion.div
+        initial={{scale: 0}}
+        animate={{scale: [0, 1, 1.5, 1]}}
+        transition={{type: "spring", duration: 1.5,delay: 0.7}}  
+        
+      >
+        <Link to="https://github.com/jfinley6" target="_blank">
+          <GitHubBtn
+            width={25}
+            height={25}
+            fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
+          />
+        </Link>
+      </motion.div>
+      <motion.div
+        initial={{
+          scale: 0,
+        }}
+        animate={{
+          scale: [0, 1.5, 2, 1],
+        }}
+        transition={{
+          type: "spring",
+          duration: 1.5,
+          delay: 0.9,
+        }}
+      >
+        <Link to="https://linkedin.com/in/john-tyler-finley" target="_blank">
+          <LinkedInBtn
+            width={25}
+            height={25}
+            fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
+          />
+        </Link>
+      </motion.div>
+      <motion.div
+        initial={{
+          scale: 0,
+        }}
+        animate={{
+          scale: [0, 1, 1.5, 1],
+        }}
+        transition={{
+          type: "spring",
+          duration: 1.5,
+          delay: 1.1,
+        }}
+      >
+        <Link to={Resume} target="_blank">
+          <ResumeBtn
+            width={25}
+            height={25}
+            fill={props.theme === "darkTheme" ? darkTheme.text : darkTheme.body}
+          />
+        </Link>
+      </motion.div>
 
-      <Line color={props.theme} />
+      <Line
+        initial={{
+          height: 0,
+        }}
+        animate={{
+          height: "8rem",
+        }}
+        transition={{
+          type: "spring",
+          duration: 1.5,
+          delay: 0.5,
+        }}
+        color={props.theme}
+      />
     </Icons>
   );
 };
