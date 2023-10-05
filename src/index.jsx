f79afa459086eee70fa6fb67b236537c273503b9
johnfinley <johnfinleybremerton@gmail.com>
2023-10-04 19:45:57 -0600

    Added react-router to root of program

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../node_modules/normalize.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
