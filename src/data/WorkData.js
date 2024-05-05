4161ad1800ad725734f6f04131ef15f986e5b0a2
johnfinley <johnfinleybremerton@gmail.com>
2024-05-05 15:11:51 -0600

    Update: add rockies double checker project to portfolio

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -4,10 +4,21 @@ import discsImg from "/out-of-bounds-discs.png"
 import redditKeywordImg from "/reddit-keyword-monitor.png"
 import portfolioImg from "/portfolio.png"
 import sidebarImg from "/animated-sidebar.png"
+import rockiesDoubleImg from "/rockies-double.png"
 
 export const Work = [
   {
     id: 1,
+    name: "Rockies Double Checker",
+    description: "A website where users can see if the Rockies got a double yesterday to get a free double cheeseburger",
+    tags: ["python", "flask"],
+
+    demo: "https://rockies-double-checker.onrender.com/",
+    github: "https://github.com/jfinley6/did-the-rockies-get-a-double-yesterday",
+    img: rockiesDoubleImg,
+  },
+  {
+    id: 2,
     name: "Animated Sidebar",
     description: "A sidebar menu built using React with slick page transitions",
     tags: ["react", "styled-components", "framer-motion"],
@@ -17,7 +28,7 @@ export const Work = [
     img: sidebarImg,
   },
   {
-    id: 2,
+    id: 3,
     name: "Out of Bounds Discs",
     description:
       "A disc golf e-commerce store built using React and Ruby on Rails",
@@ -28,7 +39,7 @@ export const Work = [
     img: discsImg,
   },
   {
-    id: 3,
+    id: 4,
     name: "React Weather Browser",
     description:
       "A weather browser built with React and incorporating Google's autocomplete API",
@@ -39,7 +50,7 @@ export const Work = [
     img: weatherBrowserImg,
   },
   {
-    id: 4,
+    id: 5,
     name: "Discussion Hub",
     description:
       "A discussion forum where you can create communities, posts, and comments",
@@ -50,7 +61,7 @@ export const Work = [
     img: discussionHubImg,
   },
   {
-    id: 5,
+    id: 6,
     name: "Reddit Keyword Monitor",
     description:
       "A tool to monitor Reddit posts for specific keywords and get email notifications for matching posts",
@@ -61,7 +72,7 @@ export const Work = [
     img: redditKeywordImg,
   },
   {
-    id: 6,
+    id: 7,
     name: "Portfolio Website",
     description: "My Portfolio Page built with React and styled-components",
     tags: ["react", "styled-components", "framer-motion"],
